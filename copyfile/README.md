5e8dda5dcaba34c8d57fa4ab932a720ee5003f6f
devbrina <sabrinamaria.dev@hotmail.com>
2023-10-08 19:23:37 -0300

    New README.md

---
+++ copyfile/README.md
@@ -0,0 +1,23 @@
+# File Copy Program
+
+This Java program copies the contents of one file to another. It takes two command-line arguments: the source file to copy from and the destination file to copy to.
+
+## Usage
+
+To run the program, use the following command:
+
+```bash
+java Main source_file destination_file
+
+
+## Notes
+
+Before using this program, please keep the following points in mind:
+
+- **Java Installation:** Make sure you have Java installed on your system. You can check if Java is installed by running `java -version` in your command prompt or terminal. If Java is not installed, you can download and install it from the official [Java website](https://www.oracle.com/java/technologies/javase-downloads.html).
+
+- **Custom File Names:** You are not limited to using the provided `entrada.txt` and `saida.txt` file names. When running the program, you can specify your own source and destination file names as needed. Just replace `source_file` and `destination_file` in the command with your preferred file names.
+
+- **Customization:** If you have specific requirements or need additional functionality, feel free to modify the Java code in `Main.java` to suit your needs. This program serves as a basic template for file copying and can be extended or adapted for various use cases.
+
+Now, you're ready to copy files with ease! Happy file copying!
--- copyfile/src/Main.java
+++ copyfile/src/Main.java
@@ -1,35 +1,42 @@
 import java.io.*;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-    try {
-        //ler um novo arquivo
-        InputStream is = new FileInputStream("entrada.txt");  //InputStream é classe abstrata mãe de FileInputStream
-        InputStreamReader ir = new InputStreamReader(is);   //classe para você não se preocupar com o formato do arquivo: converter os bytes lidos em caracteres UTF-8
-        BufferedReader br = new BufferedReader(ir); //ler as linhas de texto do arquivo
-        //escrever em um novo arquivo
-        OutputStream os = new FileOutputStream("saida.txt");
-        OutputStreamWriter or = new OutputStreamWriter(os);
-        BufferedWriter bw = new BufferedWriter(or);
-
-        String linha  = "";
-        while (true) {    //loop infinito para repetir a leitura de linhas
-            linha = br.readLine();  //le somente uma linha
-            if (linha == null)  //se ao ler, não receber nenhuma linha, ou seja, estiver nulo, faça:
-                break;
-            System.out.println(linha);
-            bw.write(linha+"\n");
+        if (args.length != 2) {
+            System.out.println("Usage: java Main source_file destination_file");
+            return;
         }
-        br.close(); // fecha o arquivo entrada
-        bw.close(); // fecha o arquivo saida
 
-    }catch (FileNotFoundException ex){
-        System.out.println("Erro na abertura do arquivo: "+ex);
-    }catch (IOException ex){
-        System.out.println("Erro na abertura do arquivo: "+ex);
-        System.out.println("fim de programa.");
-    }
+        String sourceFile = args[0];
+        String destinationFile = args[1];
+
+        try {
+            // Read the source file
+            FileInputStream is = new FileInputStream(sourceFile);
+            InputStreamReader ir = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(ir);
+
+            // Write to the destination file
+            FileOutputStream os = new FileOutputStream(destinationFile);
+            OutputStreamWriter or = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(or);
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                bw.write(line + "\n");
+            }
+
+            // Close the files
+            br.close();
+            bw.close();
+
+            System.out.println("Copy completed successfully!");
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error: Source file not found: " + sourceFile);
+        } catch (IOException ex) {
+            System.out.println("Error opening or writing to the file: " + ex);
+        }
     }
 }
